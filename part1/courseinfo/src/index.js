fc15ce17447775f6f8304fdae89ca812da4723fd
Chelsia Liu <chelsialiu@Chelsias-MacBook-Air.local>
2021-02-10 17:48:26 -0700

    Completed course information step4 refactor

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -11,7 +11,7 @@ const Content = (props) => {
   return (
     <div>
       {props.parts.map((part, index) => (
-      <Part key={index} name={part.name} exercises={part.exercises}></Part>
+        <Part key={index} name={part.name} exercises={part.exercises}></Part>
       ))}
     </div>
   )
@@ -26,31 +26,36 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  let total = 0;
+  { props.parts.map(part => total += part.exercises) }
+
   return (
-    <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {total}</p>
   )
 }
 
 const App = () => {
   const course = "Half Stack application development"
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
+  var parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
     name: 'State of a component',
     exercises: 14
-  }
+    },
+  ]
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content parts={[part1, part2, part3]}></Content>
-      <Total total={part1.exercises + part2.exercises + part3.exercises}></Total>
+      <Content parts={parts}></Content>
+      <Total parts={parts}></Total>
     </div>
   )
 }
